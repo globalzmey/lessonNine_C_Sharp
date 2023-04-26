495eb6b2c6b31295775dec2b01363039b7fe87db
Zmey <global.zmey@gmail.com>
2023-04-26 17:35:35 +0300

    Completed HW lesson 9

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+     Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int n;
+
+Console.Write("Введите целое положительное число N: ");
+n = ReadNumberFromConsole();
+Console.Write($"N = {n} -> \"");
+Console.WriteLine(NaturalNumber(n, 1) + "\"");
+
+int ReadNumberFromConsole()
+{
+  string? input = Console.ReadLine();
+  return int.Parse(input);
+}
+
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+  return m;
+}
---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 66: Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int m, n;
+
+Console.Write("Введите значение числа M: ");
+m = ReadNumberFromConsole();
+Console.Write("Введите значение числа N: ");
+n = ReadNumberFromConsole();
+
+Console.Write($"M = {m}; N = {n} -> ");
+Console.WriteLine(SumOfElements(m, n));
+
+int ReadNumberFromConsole()
+{
+  string? input = Console.ReadLine();
+  return int.Parse(input);
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
